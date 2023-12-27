e322bb027891c410f159af4dfb1aa4bc8e6fbe49
Ieva <kerseviciute.ieva@gmail.com>
2023-12-27 10:22:52 +0200

    Update sample sheet.

--- R/sample_sheet.R
+++ R/sample_sheet.R
@@ -8,14 +8,15 @@ library(openxlsx)
 data <- openxlsx::read.xlsx(snakemake@input$data, sheet = 3, colNames = FALSE) %>% setDT()
 
 data <- rbind(
-  data[ 3:12, list(SID = X1, Type = data[ 1, X1 ], Date = X2, Depth = X3, Count = X4) ],
-  data[ 3:12, list(SID = X5, Type = data[ 1, X5 ], Date = X6, Depth = X7, Count = X8) ],
-  data[ 3:14, list(SID = X9, Type = data[ 1, X9 ], Date = X10, Depth = X11, Count = X12) ],
-  data[ 3:12, list(SID = X13, Type = data[ 1, X13 ], Date = X14, Depth = X15, Count = X16) ]
+  data[ 3:12, list(SID = X1, Region = data[ 1, X1 ], Date = X2, Depth = X3, Count = X4) ],
+  data[ 3:12, list(SID = X5, Region = data[ 1, X5 ], Date = X6, Depth = X7, Count = X8) ],
+  data[ 3:14, list(SID = X9, Region = data[ 1, X9 ], Date = X10, Depth = X11, Count = X12) ],
+  data[ 3:12, list(SID = X13, Region = data[ 1, X13 ], Date = X14, Depth = X15, Count = X16) ]
 ) %>%
   .[ , AnimalID := gsub(x = SID, pattern = '(W[0-9]+)_.+', replacement = '\\1') ] %>%
   .[ , CellName := gsub(x = SID, pattern = 'W[0-9]+_(C[0-9]+)', replacement = '\\1') ] %>%
-  setcolorder(c('SID', 'AnimalID', 'CellName'))
+  data.table::setcolorder(c('SID', 'AnimalID', 'CellName')) %>%
+  .[ , Region := gsub(x = Region, pattern = '/', replacement = '') ]
 
 head(data)
 
