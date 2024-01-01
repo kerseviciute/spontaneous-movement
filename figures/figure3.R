cfab237c771c93c177c185ba347363b1858a29bf
Ieva <kerseviciute.ieva@gmail.com>
2024-01-01 16:47:11 +0200

    Update fig 3.

--- figures/figure3.R
+++ figures/figure3.R
@@ -32,6 +32,8 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
   region_label <- gsub(x = region, pattern = '_', replacement = ' ')
   region_label <- gsub(x = region_label, pattern = '23', replacement = '2/3')
 
+  max_vm <- region_average[ , max(VM) ] + 0.25
+
   p1 <- region_average %>%
     ggplot() +
     annotate('rect', xmin = -0.4, xmax = -0.2, ymin = -Inf, ymax = Inf, fill = colors[ 1 ], size = 0.5, alpha = 0.5) +
@@ -44,7 +46,12 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
     ylab('Membrane\npotential, V (mV)') +
     xlim(-0.4, 0.4) +
     ggtitle(region_label) +
-    theme(plot.title = element_text(hjust = 0.5, size = 9, face = 'bold'))
+    theme(plot.title = element_text(hjust = 0.5, size = 9, face = 'bold')) +
+    scale_x_continuous(breaks = c(-0.4, -0.2, 0, 0.2, 0.4)) +
+    annotate('text', x = -0.3, y = max_vm, label = 'B', size = 2.5) +
+    annotate('text', x = -0.05, y = max_vm, label = 'P', size = 2.5) +
+    annotate('text', x = 0.05, y = max_vm, label = 'O', size = 2.5) +
+    annotate('text', x = 0.3, y = max_vm, label = 'L', size = 2.5)
 
   type_average <- example_data[ Region == region ] %>%
     .[ , list(VM = mean(VM)), by = list(SID, Type, EventId) ] %>%
@@ -63,7 +70,10 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
     xlab('') +
     scale_colour_manual(values = c('B' = colors[ 1 ], 'P' = colors[ 2 ], 'O' = colors[ 3 ], 'L' = colors[ 4 ])) +
     stat_compare_means(
-      comparisons = list(c('B', 'P'), c('P', 'O'), c('O', 'L')),
+      comparisons = list(c('B', 'P'), c('P', 'O')),
+      method = 't.test', size = 2.5) +
+        stat_compare_means(
+      comparisons = list(c('O', 'L')),
       method = 't.test', size = 2.5) +
     theme(legend.position = 'none') +
     ylab('Average membrane potential, V (mV)')
@@ -72,7 +82,8 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
     .[ , SID := gsub(x = Trial, pattern = '.*(W[0-9]+_C[0-9]+)', replacement = '\\1') ] %>%
     merge(sample_data) %>%
     .[ , Time := Time - 0.4 ] %>%
-    .[ Region == region ]
+    .[ Region == region ] %>%
+    .[ , Trial := as.factor(Trial) %>% as.numeric() ]
 
   p3 <- ap_time %>%
     ggplot() +
@@ -87,7 +98,13 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
     theme(strip.background = element_rect(fill = 'white')) +
     theme(strip.text = element_text(colour = 'black')) +
     scale_x_continuous(breaks = c(-0.4, -0.2, 0, 0.2, 0.4)) +
-    expand_limits(y = c(0, 70))
+    expand_limits(y = c(0, 70)) +
+    annotate('text', x = -0.3, y = 69, label = 'B', size = 2.5) +
+    annotate('text', x = -0.05, y = 69, label = 'P', size = 2.5) +
+    annotate('text', x = 0.05, y = 69, label = 'O', size = 2.5) +
+    annotate('text', x = 0.3, y = 69, label = 'L', size = 2.5)
+
+  max_trial <- ap_time[ , unique(Trial) ] %>% length() + 1
 
   p4 <- ap_time %>%
     ggplot() +
@@ -103,18 +120,23 @@ for (region in c('S1_L23', 'S1_L5', 'M1_L23', 'M1_L5')) {
     theme(strip.background = element_blank()) +
     theme(strip.text = element_blank()) +
     ylab('Movement event') +
-    scale_x_continuous(breaks = c(-0.4, -0.2, 0, 0.2, 0.4))
+    annotate('text', x = -0.3, y = max_trial + max_trial * 0.08, label = 'B', size = 2.5) +
+    annotate('text', x = -0.05, y = max_trial + max_trial * 0.08, label = 'P', size = 2.5) +
+    annotate('text', x = 0.05, y = max_trial + max_trial * 0.08, label = 'O', size = 2.5) +
+    annotate('text', x = 0.3, y = max_trial + max_trial * 0.08, label = 'L', size = 2.5) +
+    theme(panel.grid = element_blank()) +
+    expand_limits(y = c(0, max_trial + max_trial * 0.1))
 
   p5 <- ggdraw() +
-    draw_plot(p1, 0, 0.83, 1, 0.16) +
-    draw_plot(p2, 0.068, 0.48, 1 - 0.068, 0.35) +
-    draw_plot(p3, 0.08, 0.25, 1 - 0.08, 0.25) +
-    draw_plot(p4, 0.138, 0, 1 - 0.138, 0.25)
+    draw_plot(p1, 0, 0.75, 1, 0.25) +
+    draw_plot(p2, 0.068, 0.4, 1 - 0.068, 0.35) +
+    draw_plot(p3, 0.08, 0.2, 1 - 0.08, 0.2) +
+    draw_plot(p4, 0.138, 0, 1 - 0.138, 0.2)
 
   # Only for the first in list
   if (region == 'S1_L23') {
     p5 <- p5 +
-      draw_plot_label(letters[ 1:4 ], x = c(0, 0, 0, 0), y = c(1, 0.83, 0.5, 0.25), size = 9)
+      draw_plot_label(letters[ 1:4 ], x = c(0, 0, 0, 0), y = c(1, 0.75, 0.4, 0.2), size = 9)
   }
 
   plot[[region]] <- p5
